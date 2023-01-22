e1188b3c9f72fcaa0e5aa6c496d1a6edb19194c8
bruno-oli <brunomaz177@gmail.com>
2023-01-22 10:24:55 -0300

    fix color bug on theme toggle button

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -14,7 +14,6 @@ const ThemeToggle = () => {
   const { isDarkTheme, setIsDarkTheme } = useContext(ThemeContext);
   return (
     <Wrapper
-      style={{filter: `${isDarkTheme ? "invert(100%)" : "invert(0%)"}`}}
       onClick={() => setIsDarkTheme(!isDarkTheme)}
       src={`${isDarkTheme ? iconSun : iconMoon}`}
       alt=""
